b4dfa52353821e2768cc4be4e2489328bb016a7f
elijah-hopper848 <ehopper848@phm.k12.in.us>
2025-12-11 11:11:06 -0500

    move gen

--- chess/constants.py
+++ chess/constants.py
@@ -1,10 +1,10 @@
-WHITE_MASK = 8
-TYPE_MASK = 7
-
 WIDTH = 8
 HEIGHT = 8
 
-DIRECTION_OFFSETS = [-WIDTH, WIDTH, -1, 1, -WIDTH - 1, -WIDTH + 1, WIDTH - 1, WIDTH + 1]
+# DIRECTION_OFFSETS = [ -WIDTH, WIDTH, -1, 1, -WIDTH - 1, -WIDTH + 1, WIDTH - 1, WIDTH + 1 ]
+# KNIGHT_OFFSETS = [-WIDTH * 2 - 1, -WIDTH * 2 + 1, -WIDTH - 2, -WIDTH + 2, WIDTH - 2, WIDTH + 2, WIDTH * 2 - 1, WIDTH * 2 + 1]
+DIRECTION_OFFSETS: list[tuple[int, int]] = [ (0, -1), (0, 1), (-1, 0), (1, 0), (-1, 1), (1, 1), (-1, -1), (1, -1) ]
+KNIGHT_OFFSETS: list[tuple[int, int]] = [ (-1, 2), (1, 2), (-2, 1), (2, 1), (-2, -1), (2, -1), (-1, -2), (1, -2) ]
 
 EMPTY = 0
 PAWN = 1
--- chess/main.py
+++ chess/main.py
@@ -3,9 +3,15 @@ from constants import *
 
 CELL_BG = AnsiStyle(bg=RGB24bit(180, 130, 40))
 CELL_ALT_BG = AnsiStyle(bg=RGB24bit(100, 50, 20))
+HIGHLIGHT_BG = AnsiStyle(bg=RGB24bit(0, 255, 127))
 
 WHITE_FG = AnsiStyle(fg=RGB24bit(255, 255, 255)).bold()
 BLACK_FG = AnsiStyle(fg=RGB24bit(20, 20, 20)).bold()
+
+def is_piece_white(piece: int) -> bool:
+	return piece & 8 != 0
+def get_piece_type(piece: int) -> int:
+	return piece & 7
 
 class ChessBoard:
 	def __init__(self) -> None:
@@ -25,21 +31,84 @@ class ChessBoard:
 		self.set_piece(target_x, target_y, self.get_piece(x, y))
 		self.set_piece(x, y, EMPTY)
 
-	def render(self) -> str:
+	def get_moves(self, x: int, y: int) -> list[int]:
+		moves = []
+
+		piece = self.get_piece(x, y)
+		piece_type = get_piece_type(piece)
+
+		if piece_type == PAWN:
+			dir = -1 if is_piece_white(piece) else 1
+
+			moves.append(self.get_index(x, y + dir))
+		elif piece_type == KNIGHT:
+			for knight_x, knight_y in KNIGHT_OFFSETS:
+				final_x, final_y = knight_x + x, knight_y + y
+
+				if self.contains(final_x, final_y):
+					moves.append(self.get_index(final_x, final_y))
+		elif piece == ROOK:
+			for dir_index in range(0, 4):
+				dir_x, dir_y = DIRECTION_OFFSETS[dir_index]
+
+				for d in range(1, 8):
+					off_x, off_y = dir_x * d, dir_y * d
+					final_x, final_y = off_x + x, off_y + y
+
+					if not self.contains(final_x, final_y):
+						break
+
+					moves.append(self.get_index(final_x, final_y))
+		elif piece == BISHOP:
+			for dir_index in range(4, 8):
+				dir_x, dir_y = DIRECTION_OFFSETS[dir_index]
+
+				for d in range(1, 8):
+					off_x, off_y = dir_x * d, dir_y * d
+					final_x, final_y = off_x + x, off_y + y
+
+					if not self.contains(final_x, final_y):
+						break
+
+					moves.append(self.get_index(final_x, final_y))
+		elif piece == QUEEN:
+			for dir_index in range(8):
+				dir_x, dir_y = DIRECTION_OFFSETS[dir_index]
+
+				for d in range(1, 8):
+					off_x, off_y = dir_x * d, dir_y * d
+					final_x, final_y = off_x + x, off_y + y
+
+					if not self.contains(final_x, final_y):
+						break
+
+					moves.append(self.get_index(final_x, final_y))
+		elif piece == KING:
+			for dir_index in range(8):
+				dir_x, dir_y = DIRECTION_OFFSETS[dir_index]
+				final_x, final_y = dir_x + x, dir_y + y
+
+				if not self.contains(final_x, final_y):
+					break
+
+				moves.append(self.get_index(final_x, final_y))
+
+		return moves
+
+	def render(self, highlight_indices: list[int]) -> str:
 		output = ""
 
 		for y in range(HEIGHT):
 			output += f"{ALPHABET[y]}  "
 
 			for x in range(WIDTH):
+				index = self.get_index(x, y)
 				piece = self.get_piece(x, y)
 
-				is_white = piece & WHITE_MASK != 0
-				piece_type = piece & TYPE_MASK
-				name = PIECE_NAMES[piece_type]
+				name = PIECE_NAMES[get_piece_type(piece)]
 
-				bg_style = CELL_ALT_BG if (x + y) & 1 != 0 else CELL_BG
-				fg_style = WHITE_FG if is_white else BLACK_FG
+				bg_style = HIGHLIGHT_BG if index in highlight_indices else CELL_ALT_BG if (x + y) & 1 != 0 else CELL_BG
+				fg_style = WHITE_FG if is_piece_white(piece) else BLACK_FG
 				
 				output += fg_style.apply(bg_style.apply_with_reset(name))
 
@@ -53,4 +122,7 @@ class ChessBoard:
 		return output
 
 board = ChessBoard()
-board.set_piece(0, 0, PAWN)
+board.set_piece(5, 5, PAWN | 8)
+
+moves = board.get_moves(5, 5)
+print(board.render(moves))
