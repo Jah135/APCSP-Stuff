26e180e78c6fbbe58038d99d9d9addf2bea1d6bb
Elijah <ehopper848@phm.k12.in.us>
2026-03-24 11:22:39 -0400

    update to wordle package

--- wordle/main.py
+++ wordle/main.py
@@ -1,27 +1,29 @@
 from pyansi import bold
 from modules.formatting import render_game
-from modules.game import WordleGame
 from modules.player import HumanWordlePlayer
+from wordle import LocalWordleGame
+from random import choice
+from modules.guesser import WordleGuesser
 
-game = WordleGame(max_guesses=6)
-player = HumanWordlePlayer(len(game.secret_word))
+game = LocalWordleGame(choice(WORD_DICTIONARY), max_guesses=6)
+player = WordleGuesser(game, PLAYABLE_DICTIONARY)
 
 print(
     f"\033[H\033[2J{bold("Simple Wordle")}\nGuess the secret {bold(str(len(game.secret_word)))} letter word!\n"
 )
 while True:
-    current_guess_index = len(game.guesses) + 1
+    current_guess_index = len(game.guess_history) + 1
     word = player.prompt_word()
 
     player.on_guess_feedback(*game.make_guess(word))
 
     print(render_game(game))
 
-    if game.is_won:
+    if game.is_win:
         print("You guessed the word!")
         break
 
-    if game.is_over:
+    if game.is_done:
         print(f"Ran out of guesses.")
         break
 
--- wordle/modules/formatting.py
+++ wordle/modules/formatting.py
@@ -1,42 +1,59 @@
 from pyansi import AnsiStyle, PaletteColor, Palette, bold
-from modules.game import WordleGame, LetterValidity
+from wordle import LocalWordleGame, LetterValidity
 
-CORRECT_STYLE = AnsiStyle(bg=Palette(PaletteColor.BrightGreen), fg=Palette(PaletteColor.Black))
-INCORRECT_STYLE = AnsiStyle(bg=Palette(PaletteColor.BrightBlack), fg=Palette(PaletteColor.Black))
-EXISTS_STYLE = AnsiStyle(bg=Palette(PaletteColor.Yellow), fg=Palette(PaletteColor.Black))
-NEUTRAL_STYLE = AnsiStyle(bg=Palette(PaletteColor.White), fg=Palette(PaletteColor.Black))
+CORRECT_STYLE = AnsiStyle(
+    bg=Palette(PaletteColor.BrightGreen), fg=Palette(PaletteColor.Black)
+)
+INCORRECT_STYLE = AnsiStyle(
+    bg=Palette(PaletteColor.BrightBlack), fg=Palette(PaletteColor.Black)
+)
+EXISTS_STYLE = AnsiStyle(
+    bg=Palette(PaletteColor.Yellow), fg=Palette(PaletteColor.Black)
+)
+NEUTRAL_STYLE = AnsiStyle(
+    bg=Palette(PaletteColor.White), fg=Palette(PaletteColor.Black)
+)
+
 
 def format_letter(char: str, validity: LetterValidity | None = None) -> str:
-	display = f" {char.upper()} "
-	if validity == None:
-		return NEUTRAL_STYLE.apply_with_reset(display)
-	elif validity == LetterValidity.Correct:
-		return CORRECT_STYLE.apply_with_reset(display)
-	elif validity == LetterValidity.Exists:
-		return EXISTS_STYLE.apply_with_reset(display)
-	return INCORRECT_STYLE.apply_with_reset(display)
+    display = f" {char.upper()} "
+    if validity == None:
+        return NEUTRAL_STYLE.apply_with_reset(display)
+    elif validity == LetterValidity.Correct:
+        return CORRECT_STYLE.apply_with_reset(display)
+    elif validity == LetterValidity.Exists:
+        return EXISTS_STYLE.apply_with_reset(display)
+    return INCORRECT_STYLE.apply_with_reset(display)
+
 
 def format_guess(guess: str, guess_validity: list[LetterValidity]) -> str:
-	return "".join(format_letter(char, validity) for char, validity in zip(guess, guess_validity))
+    return "".join(
+        format_letter(char, validity) for char, validity in zip(guess, guess_validity)
+    )
 
-def render_game(game: WordleGame) -> str:
-	output = "\x1b[2J\x1b[H\n"
 
-	for guess_index, guess in enumerate(game.guesses):
-		word, word_validity = guess
+def render_game(game: LocalWordleGame) -> str:
+    output = "\x1b[2J\x1b[H\n"
 
-		output += f"{guess_index + 1} > {format_guess(word, word_validity)}\n"
+    for guess_index, guess in enumerate(game.guess_history):
+        word, word_validity = guess
 
-	output += "\n"
+        output += f"{guess_index + 1} > {format_guess(word, word_validity)}\n"
 
-	return output
+    output += "\n"
 
-KEYBOARD = ("qwertyuiop","asdfghjkl","zxcvbnm")
+    return output
+
+
+KEYBOARD_ROWS = ("qwertyuiop", "asdfghjkl", "zxcvbnm")
+
 
 def render_keyboard(validity: dict[str, LetterValidity]):
-	output = ""
+    output = ""
 
-	for row in KEYBOARD:
-		output += ("".join(format_letter(letter, validity.get(letter)) for letter in row)) + "\n"
+    for row in KEYBOARD_ROWS:
+        output += (
+            "".join(format_letter(letter, validity.get(letter)) for letter in row)
+        ) + "\n"
 
-	return output
+    return output
