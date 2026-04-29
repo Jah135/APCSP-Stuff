7d1c9717b482678d79cb9fb2f586d2d169e5b5dc
Elijah <ehopper848@phm.k12.in.us>
2026-04-29 11:10:03 -0400

    squares that march maybe

--- marching/main.py
+++ marching/main.py
@@ -1,31 +1,29 @@
-"""
-B---2---C
-|       |
-1       3
-|       |
-A---0---D
+from pygame.surface import Surface
+import pygame.draw as draw
 
-A = 1
-B = 2
-C = 4
-D = 8
-"""
+from game import Game
 
-COUNTER_VERTEX_INDICES: list[list[int]] = [
-    [],  # 0
-    [0, 1],  # 1
-    [1, 2],  # 2
-    [0, 2],  # 3
-    [2, 3],  # 4
-    [0, 1, 2, 3],  # 5
-    [1, 3],  # 6
-    [0, 3],  # 7
-    [0, 3],  # 8
-    [1, 3],  # 9
-    [1, 2, 0, 3],  # 10
-    [2, 3],  # 11
-    [0, 2],  # 12
-    [1, 2],  # 13
-    [0, 1],  # 14
-    [],  # 15
-]
+pos = (100, 100)
+vel = (50, -130)
+
+
+class TestGame(Game):
+    window_width = 500
+    window_height = 500
+    target_framerate = -1
+
+    def on_draw(self, out: Surface):
+        out.fill("black")
+
+        draw.circle(out, "pink", pos, 2)
+        draw.line(out, "blue", pos, (pos[0] + vel[0], pos[1] + vel[1]))
+
+    def on_update(self, dt: float):
+        global pos, vel
+
+        pos = (pos[0] + vel[0] * dt, pos[1] + vel[1] * dt)
+        vel = (vel[0], vel[1] + 60 * dt)
+
+
+game = TestGame()
+game.start()
